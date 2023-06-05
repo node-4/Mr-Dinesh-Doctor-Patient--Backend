c6c0cbff254fac620221cde5211c09fc79b6103a
rubiyadav <rubiflyeis@gmail.com>
2023-06-05 19:31:08 +0530

    update

--- routes/preferenceRouter.js
+++ routes/preferenceRouter.js
@@ -3,9 +3,9 @@ const preferenceController = require("../Controllers/preferenceController");
 const authJwt = require("../Middlewares/authJwt");
 
 router.post('/addpreference',[authJwt.verifyToken], preferenceController.addpreference);
-router.get('/getpreferencebytestid/:testID',[authJwt.verifyToken], getpreferencebytestid);
-router.get('/getpreferencebyid/:id',[authJwt.verifyToken], getpreferencebyid);
-router.get('/getallpreference',[authJwt.verifyToken], getallpreference);
-router.delete('/deletepreference/:id',[authJwt.verifyToken], deletepreference);
-router.put('/editpreference/:id',[authJwt.verifyToken], editpreference);
+router.get('/getpreferencebytestid/:testID',[authJwt.verifyToken], preferenceController.getpreferencebytestid);
+router.get('/getpreferencebyid/:id',[authJwt.verifyToken], preferenceController.getpreferencebyid);
+router.get('/getallpreference',[authJwt.verifyToken], preferenceController.getallpreference);
+router.delete('/deletepreference/:id',[authJwt.verifyToken], preferenceController.deletepreference);
+router.put('/editpreference/:id',[authJwt.verifyToken], preferenceController.editpreference);
 module.exports = router;
